08586ff5791e7e1696ab842cdd6991c6957ee675
AeroBackup <96149846+AeroBackup@users.noreply.github.com>
2022-01-25 10:22:34 -0800

    Use Luau one line if statement instead of "and/or" ternary

--- src/server/Commands/promptServer.lua
+++ src/server/Commands/promptServer.lua
@@ -3,5 +3,5 @@ local Market = game:GetService("MarketplaceService")
 return function(_, Player, AssetNumber)
 	local Success, Error = pcall(Market.PromptPurchase, Market, Player, AssetNumber)
 
-	return Success and "Prompted " .. Player.Name .. " with asset." or "Error: " .. Error
+	return if Success then "Prompted " .. Player.Name .. " with asset." else "Error: " .. Error
 end
