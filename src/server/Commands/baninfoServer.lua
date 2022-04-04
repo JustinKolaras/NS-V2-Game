4e44ddc8636f9b8aa7dcbeb9dff4c025c6d2f8cf
Aerosphia <justinkolaras@outlook.com>
2022-04-03 20:31:27 -0700

    Receive Executor

--- src/server/Commands/baninfoServer.lua
+++ src/server/Commands/baninfoServer.lua
@@ -5,15 +5,17 @@ local BanService = require(ServerStorage.Storage.Modules.BanService)
 
 return function(_, Player)
 	local PlayerBanned, BanReason, ExecutorId, Date, System = BanService:GetBanInfo(Player)
+	local ExecutorName = Players:GetNameFromUserIdAsync(ExecutorId)
+	local PlayerName = Players:GetNameFromUserIdAsync(Player)
 
 	if not PlayerBanned then
-		return Players:GetNameFromUserIdAsync(Player) .. " is not banned."
+		return PlayerName .. " is not banned."
 	end
 
 	return ('%s was banned on %s by %s for: "%s"'):format(
-		Players:GetNameFromUserIdAsync(Player),
+		PlayerName,
 		Date,
-		if System then "System" else Players:GetNameFromUserIdAsync(ExecutorId),
+		if System then "System" else ExecutorName,
 		BanReason
 	)
 end
--- src/server/RemoteAPI/UnbanAPI.lua
+++ src/server/RemoteAPI/UnbanAPI.lua
@@ -1,8 +1,9 @@
+local Players = game:GetService("Players")
 local Http = game:GetService("HttpService")
 local ServerStorage = game:GetService("ServerStorage")
 
 local BanService = require(ServerStorage.Storage.Modules.BanService)
--- local GameModLogs = require(ServerStorage.Storage.WebhookPresets.GameModLogs)
+local GameModLogs = require(ServerStorage.Storage.WebhookPresets.GameModLogs)
 local secrets = require(ServerStorage.Storage.Modules.secrets)
 
 local Endpoints = {
@@ -18,7 +19,8 @@ return function()
 		data = Http:JSONDecode(data)
 		if data.status == "ok" then
 			for _, dict in ipairs(data.data) do
-				local id = dict.toUnbanID
+				local id, executor = dict.toUnbanID, dict.executor
+				local executorName = Players:GetNameFromUserIdAsync(executor)
 
 				-- If the ban doesn't exist, we still will want to remove it.
 
@@ -30,6 +32,17 @@ return function()
 						["Authorization"] = secrets["NS_API_AUTHORIZATION"],
 					},
 				})
+
+				-- Send to mod log channel
+				local err, result = GameModLogs:SendUnban({
+					ExecutorName = executorName,
+					VictimName = Players:GetNameFromUserIdAsync(id),
+					VictimID = id,
+				})
+
+				if err then
+					warn(result)
+				end
 
 				-- Remove from Roblox DataStore
 				local apiResult = BanService:Remove(id)
